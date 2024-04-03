bbf45777628bce2e1355f9307f010b3415207eb7
Faisal2506 <shahfaiz3105@gmail.com>
2024-04-03 16:38:10 +0530

    Display number of items on header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+const cartData = useSelector((state)=>state.reducer)
+// console.warn(cartData);
+const [cartItems,setCartItems] = React.useState(0)
+React.useEffect(() => {
+  setCartItems(cartData.length)
+}, [cartData])
+
   return (
-    <View>
-      <Text style={{textAlign:"right",padding:5,fontSize:20,backgroundColor:"orange"}}>0</Text>
+    <View style={{height:40,width:40,borderRadius:20,alignSelf:"flex-end",backgroundColor:"orange",alignItems:"center",justifyContent:"center"}}>
+      <Text style={{fontSize:25}}>{cartItems}</Text>
     </View>
   )
 }
