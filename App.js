c4530cc9b2d05edfa6817f037359bbbd7e30b9fa
Faisal2506 <shahfaiz3105@gmail.com>
2024-04-03 14:41:50 +0530

    Basic UI done

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Header from './app/components/Header';
+import Product from './app/components/Product';
 
 export default function App() {
+
+  const products =[
+    {
+      name:"Samsung Mobile",
+      color:"white",
+      price:30000,
+      image:require('./assets/mobile_icon.webp')
+    },
+    {
+      name:"Apple Mobile",
+      color:"black",
+      price:130000,
+      image:require('./assets/mobile_icon.webp')
+    },
+    {
+      name:"Nokia Mobile",
+      color:"green",
+      price:20000,
+      image:require('./assets/mobile_icon.webp')
+    }
+  ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <ScrollView>
+      {
+        products.map((item)=>
+          <Product item={item} />
+        )
+      }
+      </ScrollView>
+      
+      {/* <Text>UI for add to cart with Redux</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +45,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:20,
+    paddingLeft:20
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
